a0c43946c6fb53159a68f2ff4f53e6770c2a11ed
Matt <mattchavez815@gmail.com>
2025-11-15 16:56:10 -0500

    deployment 2

--- Kambaz/Database/Enrollments/dao.js
+++ Kambaz/Database/Enrollments/dao.js
@@ -1,53 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
-
-let enrollments = [];
-
-export const createEnrollment = (enrollment) => {
-  const newEnrollment = {
-    ...enrollment,
-    _id: uuidv4(),
-  };
-  enrollments.push(newEnrollment);
-  return newEnrollment;
-};
-
-export const findAllEnrollments = () => enrollments;
-
-export const findEnrollmentById = (id) => {
-  return enrollments.find((enrollment) => enrollment._id === id);
-};
-
-export const findEnrollmentsByUser = (userId) => {
-  return enrollments.filter((enrollment) => enrollment.user === userId);
-};
-
-export const findEnrollmentsByCourse = (courseId) => {
-  return enrollments.filter((enrollment) => enrollment.course === courseId);
-};
-
-export const findEnrollmentByUserAndCourse = (userId, courseId) => {
-  return enrollments.find(
-    (enrollment) => enrollment.user === userId && enrollment.course === courseId
-  );
-};
-
-export const deleteEnrollment = (id) => {
-  const index = enrollments.findIndex((e) => e._id === id);
-  if (index === -1) {
-    return false;
+export default function EnrollmentsDao(db) {
+  function enrollUserInCourse(userId, courseId) {
+    const { enrollments } = db;
+    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
   }
-  enrollments.splice(index, 1);
-  return true;
-};
-
-export const deleteEnrollmentByUserAndCourse = (userId, courseId) => {
-  const index = enrollments.findIndex(
-    (e) => e.user === userId && e.course === courseId
-  );
-  if (index === -1) {
-    return false;
-  }
-  enrollments.splice(index, 1);
-  return true;
-};
-
+  return { enrollUserInCourse };
+}
--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import express from "express";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
-import AssignmentRoutes from "./Kambaz/Database/Assignments/routes.js";
-import EnrollmentRoutes from "./Kambaz/Database/Enrollments/routes.js";
-
+import db from "./Kambaz/Database/index.js";
+import UserRoutes from "./Kambaz/Users/routes.js";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
+import "dotenv/config";
+import session from "express-session";
 const app = express();
-app.use(cors()); 
+UserRoutes(app, db);
+CourseRoutes(app, db);
+app.use(cors({
+   credentials: true,
+   origin: process.env.CLIENT_URL || "http://localhost:3000",
+   
+ }
+)); 
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());                   
-Lab5(app);
-AssignmentRoutes(app);
-EnrollmentRoutes(app);
+Lab5(app);                         
 app.listen(4000);
