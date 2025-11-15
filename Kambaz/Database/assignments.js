68c3f25f5506a6f6e7f5f9e21c37de29629be01d
Matt <mattchavez815@gmail.com>
2025-11-15 16:55:49 -0500

    deployment

---
+++ Kambaz/Database/assignments.js
@@ -0,0 +1,101 @@
+export default [
+  {
+    "id": "A101",
+    "title": "Propulsion Assignment",
+    "course": "RS101",
+    "description": "",
+    "availableFrom": "2023-11-02T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-09T23:59",
+    "points": 100
+  },
+  {
+    "id": "A102",
+    "title": "Combustion Analysis",
+    "course": "RS101",
+    "description": "",
+    "availableFrom": "2023-11-12T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-19T23:59",
+    "points": 50
+  },
+  {
+    "id": "A103",
+    "title": "Nozzle Design Project",
+    "course": "RS101",
+    "description": "",
+    "availableFrom": "2023-11-21T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-29T23:59",
+    "points": 150
+  },
+  {
+    "id": "A201",
+    "title": "Aerodynamics Quiz",
+    "course": "RS102",
+    "description": "",
+    "availableFrom": "2023-11-03T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-03T23:59",
+    "points": 120
+  },
+  {
+    "id": "A202",
+    "title": "Flow Analysis",
+    "course": "RS102",
+    "description": "",
+    "availableFrom": "2023-11-06T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-15T23:59",
+    "points": 60
+  },
+  {
+    "id": "A203",
+    "title": "Heating Analysis",
+    "course": "RS102",
+    "description": "",
+    "availableFrom": "2023-11-12T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-15T23:59",
+    "points": 85
+  },
+  {
+    "id": "A301",
+    "title": "Structural Design Task",
+    "course": "RS103",
+    "description": "",
+    "availableFrom": "2023-11-20T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-22T23:59",
+    "points": 25
+  },
+  {
+    "id": "A302",
+    "title": "Orbital Calculations",
+    "course": "RS103",
+    "description": "",
+    "availableFrom": "2023-11-09T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-14T23:59",
+    "points": 100
+  },
+  {
+    "id": "A303",
+    "title": "Systems Engineering Exam",
+    "course": "RS103",
+    "description": "",
+    "availableFrom": "2023-11-10T00:00",
+    "availableUntil": "",
+    "assignTo": "",
+    "dueDate": "2023-11-11T23:59",
+    "points": 300
+  }
+]
